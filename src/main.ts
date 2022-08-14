40d125041d18bbfa37badb7edf2457cdb8ce3f5f
RiverRay <laolei@forkway.cn>
2022-08-14 13:54:19 +0800

    feat: init project

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as requestIp from 'request-ip';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // CORS is enabled
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Request Validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.use(requestIp.mw());
+
+  // Helmet Middleware against known security vulnerabilities
+  app.use(helmet());
+
+  // Swagger API Documentation
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Hackathon Starter by @ahmetuysal')
+    .setDescription('NestJS Hackathon Starter API description')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000, '127.0.0.1');
+}
+
+bootstrap();
